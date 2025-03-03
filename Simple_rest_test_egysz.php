3790b9c4f1279dd3cb27c96939fa39b5903bacf5
steyerzalan <steyerzalan@gmail.com>
2025-03-03 13:06:52 +0100

    MobileRestHandler.php elkészült

--- Simple_rest_test_egysz.php
+++ Simple_rest_test_egysz.php
@@ -1,25 +1,25 @@
 <?php
-
 require_once('SimpleRest.php');
 
-//Tesztelés JSON válasz helyes beállításával
-function testSimpleRest()
-{
-    //Létrehozzuk a SimpleRest osztály példányát
+// Tesztelés JSON válasz helyes beállításával
+function testSimpleRest() {
+    // Létrehozzuk a SimpleRest osztály példányát
     $simpleRestObj = new SimpleRest();
-
-    //Set HTTP fejlécet először, majd küldj JSON választ
+    
+    // Set HTTP fejlécet először, majd küldj JSON választ
     $simpleRestObj->setHttpHeaders("application/json", 200);
-
-    //További JSON válasz adat
+    
+    // További JSON válasz adat
     $responseData = array(
-        "status" => "ok",
+        "status" => "OK", 
         "message" => "Sikeres válasz.",
-        "http_status_message" => $simpleRestObj->getHttpStatusMessage(200)
+        "http_status_message" => $simpleRestObj->getHttpStatusMessage(200) // HTTP státuszkód üzenet
     );
     
-    //JSON válasz küldése
-    testSimpleRest();
+    // JSON válasz küldése
+    echo json_encode($responseData);
 }
 
+// Teszt futtatása
+testSimpleRest();
 ?>
